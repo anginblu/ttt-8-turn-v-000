cc1088e2c28ab3cdb7aa6cb9dccfef5d0ce0d76b
Jacqueline Lee <anginblu@gmail.com>
2017-10-05 22:04:38 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -10,7 +10,7 @@ def input_to_index(input)
   input.to_i - 1
 end
 
-def valid_move?(board = [" ", " ", " ", " ", " ", " ", " ", " ", " "], index)
+def valid_move?(board, index)
   if 0 > index || index > 8 || board[index] == "X" || board[index] == "O"
     false
   else
